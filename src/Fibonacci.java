27b7c01d7ffc4241df483cc73fb843fa00c89c0b
Rachel MacDougall <macdougallrachel03@gmail.com>
2022-09-04 14:20:21 -0700

    Commit #2 Version that actually requires recursion.

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -2,28 +2,24 @@ import java.util.ArrayList;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int num = 8;
-        ArrayList<Integer> sequence = new ArrayList<Integer>();
+        int num = 8;    // num is the number of indices in the Fibonacci Sequence.
 
-        for (int index = 1; index <= num; index++) {
-            sequence.add(FibSeq(index, sequence));
-        }
-
-        for (int index = 0; index < num; index++) {
-            System.out.print(sequence.get(index));
-            if (index < (num - 1)) {
+        for (int index = 1; index <= num; index++) {    // Labelled indices starting at 1 for better readability.
+            System.out.print(FibSeq(index));    // Calling the FibSeq method and printing numbers on the same line.
+            if (index < num) {  // Prints a comma and space after numbers in the Fibonacci Sequence, but never after the last number.
                 System.out.print(", ");
             }
         }
     }
 
-    private static int FibSeq(int i, ArrayList<Integer> numList) {
-        int num = 1;
+    private static int FibSeq(int i) {
+        int result = 1;
 
+        // If it is not the first two indices in the list, it adds the values of the two previous indices. Otherwise, it returns 1.
         if (i >= 3) {
-            num = FibSeq(i - 1, numList) + FibSeq(i - 2, numList);
+            result = FibSeq(i - 1) + FibSeq(i - 2);
         }
 
-        return num;
+        return result;
     }
 }
